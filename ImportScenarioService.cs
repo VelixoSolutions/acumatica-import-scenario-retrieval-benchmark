d220f97fc74dcdd13acd2de2dd1b03080851e7da
Sardor Shukurov <sshukurov@velixo.com>
2021-12-29 22:26:04 +0500

    Add benchmark project

---
+++ ImportScenarioService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcumaticaSoap;
+
+namespace AcumaticaImportScenarioRetrievalBenchmark
+{
+    public class ImportScenarioService
+    {
+        private const string ImportScenariosScreenID = "SM206025";
+        private const string ScenarioView = "Mappings";
+
+        public async Task<IReadOnlyCollection<ImportScenario>> GetScenariosWithScreenApiAsync(
+            string acumaticaUrl,
+            string tenant,
+            string username,
+            string password)
+        {
+            var soapLoginService = new SoapLoginService();
+            var soap = await soapLoginService.LoginAsync(acumaticaUrl, tenant, username, password);
+
+            var commands = new Command[]
+            {
+                new Field { ObjectName = ScenarioView, FieldName = "Name" },
+                new Field { ObjectName = ScenarioView, FieldName = "ScreenID" },
+                new Field { ObjectName = ScenarioView, FieldName = "IsActive" },
+            };
+
+            var request = new ExportRequest(
+                ImportScenariosScreenID,
+                commands,
+                filters: null,
+                topCount: 0,
+                includeHeaders: false,
+                breakOnError: true);
+
+            ExportResponse results = await soap.ExportAsync(request);
+
+            await soap.LogoutAsync();
+
+            return results.ExportResult
+                .Where(line => string.Equals(line[2], bool.TrueString, System.StringComparison.OrdinalIgnoreCase))
+                .Select(line => new ImportScenario(line[0], line[1])).ToArray();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AcumaticaImportScenarioRetrievalBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This console app measures time to retrieve import scenarios from an Acumatica screen-based SOAP API.");
+            Console.WriteLine();
+
+            Console.Write("Please type in URL to Acumatica instance: ");
+            string acumaticaUrl = Console.ReadLine();
+
+            Console.Write("Tenant: ");
+            string tenant = Console.ReadLine();
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Running benchmark...");
+            Console.WriteLine();
+
+            RunBenchmark(acumaticaUrl, tenant, username, password)
+                .GetAwaiter().GetResult();
+
+            Console.WriteLine();
+            Console.WriteLine("Finished!");
+            Console.ReadKey();
+        }
+
+        private static async Task RunBenchmark(
+            string acumaticaUrl,
+            string tenant,
+            string username,
+            string password)
+        {
+            BenchmarkInfo firstBenchmark = await BenchmarkRetrievalOfImportScenariosAsync(
+                acumaticaUrl,
+                tenant, 
+                username, 
+                password);
+
+            Console.WriteLine(
+                $"First attempt: {ConvertToSeconds(firstBenchmark.Milliseconds)} sec. " +
+                $"Retrieved import scenarios: {firstBenchmark.ImportScenarios.Count}");
+
+            BenchmarkInfo secondBenchmark = await BenchmarkRetrievalOfImportScenariosAsync(
+                acumaticaUrl,
+                tenant, 
+                username, 
+                password);
+
+            Console.WriteLine(
+                $"Second attempt: {ConvertToSeconds(secondBenchmark.Milliseconds)} sec. " +
+                $"Retrieved import scenarios: {secondBenchmark.ImportScenarios.Count}");
+        }
+
+        private static int ConvertToSeconds(long milliseconds) => (int)Math.Round(milliseconds / (decimal)1000);
+
+        private static async Task<BenchmarkInfo> BenchmarkRetrievalOfImportScenariosAsync(
+            string acumaticaUrl,
+            string tenant,
+            string username,
+            string password)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            var importScenarioService = new ImportScenarioService();
+            var importScenarios = await importScenarioService
+                .GetScenariosWithScreenApiAsync(acumaticaUrl, tenant, username, password)
+                .ConfigureAwait(false);
+
+            stopwatch.Stop();
+            return new BenchmarkInfo(stopwatch.ElapsedMilliseconds, importScenarios);
+        }
+
+        private class BenchmarkInfo
+        {
+            public long Milliseconds { get; }
+            public IReadOnlyCollection<ImportScenario> ImportScenarios { get; }
+
+            public BenchmarkInfo(long milliseconds, IReadOnlyCollection<ImportScenario> importScenarios)
+            {
+                Milliseconds = milliseconds;
+                ImportScenarios = importScenarios;
+            }
+        }
+    }
+}
